dca7dea99a63a5f6dd2410a56da332bf31aa93d4
Rupesh Sigdel <rupeshcgdl2060@gmail.com>
2024-07-10 00:42:56 +0545

    new update GEMINI

--- AI_Excel/settings.py
+++ AI_Excel/settings.py
@@ -14,13 +14,10 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load environment variables from the .env file
 load_dotenv()
 
-# Get the Gemini API key from environment variables
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 GEMINI_API_URL = f'https://console.cloud.google.com/apis/api/sheets.googleapis.com/metrics?project=aiexcel-1720019031726'
 
@@ -29,10 +26,8 @@ if GEMINI_API_KEY is None:
     raise ValueError("GEMINI_API_KEY environment variable is not set")
 
 
-# Get the secret key from environment variables (replace 'YOUR_SECRET_KEY' with your actual key or load it from the .env file)
 SECRET_KEY = os.getenv('SECRET_KEY', 'asfhdksadgkalntylksjth')
 
-# Set debug mode
 DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
 import requests
